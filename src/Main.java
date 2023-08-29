f832602e908bb6af19fdf44d5e12cb0dcfb81689
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2023-08-29 16:16:11 +0200

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sumTotal = 0;
+        int counter = 0;
+        for(int i = 1; counter < 5 && i <=1000; i++){
+            if((i % 3 == 0) && (i % 5 == 0)){
+                sumTotal += i;
+                System.out.println("Number " + i + " has met the conditions");
+                counter++;
+            }
+        }
+        System.out.println("Total sum of 5 numbers that met the conditions equals " + sumTotal);
     }
 }
